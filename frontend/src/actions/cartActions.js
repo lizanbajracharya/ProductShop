17506a5c0e8ddf8fe946b9a2f9535aaa69110cdc
lizanbajracharya <lizan.bajra97@gmail.com>
2021-05-05 19:08:18 +0545

    item added in cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { CART_ADD_ITEM } from '../contants/cartConstants'
 
-export const addToCart = (id, qty) => async (dispactch, getState) => {
+export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
 
-  dispatchEvent({
+  dispatch({
     type: CART_ADD_ITEM,
     payload: {
       product: data._id,
