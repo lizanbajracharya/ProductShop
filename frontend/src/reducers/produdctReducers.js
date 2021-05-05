e9e6be0b9b723d88de3ac362220594e0d64ee632
lizanbajracharya <lizan.bajra97@gmail.com>
2021-05-05 17:54:45 +0545

    Product detail redux state implemented

--- frontend/src/reducers/produdctReducers.js
+++ frontend/src/reducers/produdctReducers.js
@@ -1,4 +1,11 @@
-import {PRODUCT_LIST_SUCCESS,PRODUCT_LIST_REQUEST,PRODUCT_LIST_FAIL} from '../contants/productContacts'
+import {
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_FAIL,
+} from '../contants/productContacts'
 
 export const productListReducer = (state = { products: [] }, action) => {
   const { type, payload } = action
@@ -15,6 +22,33 @@ export const productListReducer = (state = { products: [] }, action) => {
         products: payload,
       }
     case PRODUCT_LIST_FAIL:
+      return {
+        loading: false,
+        error: payload,
+      }
+    default:
+      return state
+  }
+}
+
+export const productDetailReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      }
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: payload,
+      }
+    case PRODUCT_DETAILS_FAIL:
       return {
         loading: false,
         error: payload,
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/produdctReducers'
+import {
+  productListReducer,
+  productDetailReducer,
+} from './reducers/produdctReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetail: productDetailReducer,
 })
 
 const initialState = {}
