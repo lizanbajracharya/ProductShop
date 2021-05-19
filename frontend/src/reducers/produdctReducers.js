5d6495bb48aca5b9652fe9351a3531603f1af666
lizanbajracharya <lizan.bajra97@gmail.com>
2021-05-20 00:31:52 +0545

    Product edit done

--- frontend/src/reducers/produdctReducers.js
+++ frontend/src/reducers/produdctReducers.js
@@ -12,6 +12,10 @@ import {
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_REQUEST,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_UPDATE_RESET,
 } from '../contants/productContacts'
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -109,6 +113,32 @@ export const productCreateReducer = (state = {}, action) => {
       }
     case PRODUCT_CREATE_RESET:
       return {}
+    default:
+      return state
+  }
+}
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case PRODUCT_UPDATE_REQUEST:
+      return {
+        loading: true,
+      }
+    case PRODUCT_UPDATE_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        product: payload,
+      }
+    case PRODUCT_UPDATE_FAIL:
+      return {
+        loading: false,
+        error: payload,
+      }
+    case PRODUCT_UPDATE_RESET:
+      return { product: {} }
     default:
       return state
   }
