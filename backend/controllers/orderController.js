babc4af4825890693a8cc1266f4859e14df988ac
lizanbajracharya <lizan.bajra97@gmail.com>
2021-05-19 13:36:00 +0545

    order dispaly done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -60,16 +60,16 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id)
 
   if (order) {
-    order.isPaid=true
-    order.paidAt=Date.now()
-    order.paymentResult={
-      id:req.body.id,
-      status:req.body.status,
-      update_time:req.body.update_time,
-      email_address:req.body.payer.email_address
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
     }
 
-    const updatedOrder= await order.save()
+    const updatedOrder = await order.save()
 
     res.json(updatedOrder)
   } else {
@@ -78,4 +78,13 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems,getOrderById,updateOrderToPaid }
+// @desc     Get logged in user order
+// @route    GET api/orders/myorders
+// @access   Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id })
+
+  res.json(orders)
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -101,7 +101,7 @@ const orderSchema = mongoose.Schema(
     },
   },
   {
-    timestamp: true,
+    timestamps: true,
   }
 )
 
