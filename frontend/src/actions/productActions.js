8999da514bde46dfb749411066c290e30dbeeabc
lizanbajracharya <lizan.bajra97@gmail.com>
2021-05-20 19:18:14 +0545

    Carousel done

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -17,6 +17,9 @@ import {
   PRODUCT_CREATE_REVIEW_REQUEST,
   PRODUCT_CREATE_REVIEW_SUCCESS,
   PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_TOP_REQUEST,
+  PRODUCT_TOP_SUCCESS,
+  PRODUCT_TOP_FAIL,
 } from '../contants/productContacts'
 import axios from 'axios'
 
@@ -191,6 +194,27 @@ export const createProductReview =
     } catch (error) {
       dispatch({
         type: PRODUCT_CREATE_REVIEW_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      })
+    }
+  }
+
+  export const listTopProducts = () => async (dispatch) => {
+    try {
+      dispatch({ type: PRODUCT_TOP_REQUEST })
+  
+      const { data } = await axios.get(`/api/products/top`)
+  
+      dispatch({
+        type: PRODUCT_TOP_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_TOP_FAIL,
         payload:
           error.response && error.response.data.message
             ? error.response.data.message
